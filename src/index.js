8953c8583dfacda607c46a79fb9e1c752cec08c6
daprieu <daprieu@gmail.com>
2021-05-07 14:53:25 -0500

    added code from level up

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { GameRater } from './components/RaterProject';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <GameRater />
   </React.StrictMode>,
   document.getElementById('root')
 );
